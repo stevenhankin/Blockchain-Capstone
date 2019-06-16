e92e6f9c0b8f2d5191f1d6267f0c843f34023463
Steven Hankin <steven.hankin@gmail.com>
2019-06-16 13:54:02 +0100

    feat: Verifier compiled and tested

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,43 +1,59 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-// TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
-
+import "./ERC721Mintable.sol";
 
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is ERC721Mintable {
+
+    SquareVerifier squareVerifier;
+
+    constructor(address verifierAddress, string memory name, string memory symbol) ERC721Mintable(name, symbol) public
+    {
+        // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+        squareVerifier = SquareVerifier(verifierAddress);
+    }
+
+    // TODO define a solutions struct that can hold an index & an address
+    struct solution {
+        uint _index;
+        address _address;
+    }
+
+//     TODO define an array of the above struct
+//    solutions[] storage mySolutions;
+
+    // TODO define a mapping to store unique solutions submitted
+    /**
+     * @dev mapping to store unique solutions submitted
+     */
+    mapping(uint => solution) private _solutions;
 
 
-
-// TODO define a solutions struct that can hold an index & an address
-
-
-// TODO define an array of the above struct
+    // TODO Create an event to emit when a solution is added
+    event Solution (uint hash);
 
 
-// TODO define a mapping to store unique solutions submitted
+    // TODO Create a function to add the solutions to the array and emit the event
 
-
-
-// TODO Create an event to emit when a solution is added
-
-
-
-// TODO Create a function to add the solutions to the array and emit the event
-
+//    function addSolution () {
+//        _solutions[_hash] = ();
+//        emit Solution(hash);
+//    }
 
 
 // TODO Create a function to mint new NFT only after the solution has been verified
 //  - make sure the solution is unique (has not been used before)
 //  - make sure you handle metadata as well as tokenSuplly
 
-  
+
+}
 
 
+// Interface
+contract SquareVerifier {
 
-
-
-
-
+}
 
 
 
--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,12 +1,14 @@
 // migrating the appropriate contracts
 const ERC721Mintable = artifacts.require("./ERC721Mintable.sol");
-// const SquareVerifier = artifacts.require("./SquareVerifier.sol");
-// const SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+const SquareVerifier = artifacts.require("./SquareVerifier.sol");
+const SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 module.exports = function(deployer, network, accounts) {
   const name = "House Token";
   const symbol = "HTX";
   deployer.deploy(ERC721Mintable,name, symbol);
-  // deployer.deploy(SquareVerifier);
-  // deployer.deploy(SolnSquareVerifier);
+  deployer.deploy(SquareVerifier).then( () => {
+    console.log('SquareVerifier.address',SquareVerifier.address);
+    return deployer.deploy(SolnSquareVerifier, SquareVerifier.address, name, symbol);
+  });
 };
